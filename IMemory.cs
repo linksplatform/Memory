7e53eb597ba648284fd5e9151f938e4bcca072db
Константин Дьяченко <konard@me.com>
2019-07-20 21:26:06 +0300

    Initial code commit, forked from: https://github.com/Konard/LinksPlatform

---
+++ IMemory.cs
@@ -0,0 +1,15 @@
+﻿namespace Platform.Memory
+{
+    /// <summary>
+    /// Represents a memory block interface with size in bytes.
+    /// Представляет интерфейс блока памяти с размером в байтах.
+    /// </summary>
+    public interface IMemory
+    {
+        /// <summary>
+        /// Gets the size in bytes of this memory block.
+        /// Возвращает размер блока памяти в байтах.
+        /// </summary>
+        long Size { get; }
+    }
+}
