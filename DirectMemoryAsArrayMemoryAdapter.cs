b03b8a7b8514dfe12604540062769e4d68a36414
Константин Дьяченко <konard@me.com>
2019-09-03 13:16:18 +0300

    No more usage of obsolete methods.

--- DirectMemoryAsArrayMemoryAdapter.cs
+++ DirectMemoryAsArrayMemoryAdapter.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using Platform.Disposables;
 using Platform.Exceptions;
-using Platform.Unsafe;
 
 namespace Platform.Memory
 {
@@ -10,7 +9,7 @@ namespace Platform.Memory
     /// <para>Представляет адаптер к блоку памяти с доступом через индексатор.</para>
     /// </summary>
     /// <typeparam name="TElement"><para>Element type.</para><para>Тип элемента.</para></typeparam>
-    public class DirectMemoryAsArrayMemoryAdapter<TElement> : DisposableBase, IArrayMemory<TElement>, IDirectMemory
+    public unsafe class DirectMemoryAsArrayMemoryAdapter<TElement> : DisposableBase, IArrayMemory<TElement>, IDirectMemory
         where TElement : struct
     {
         #region Fields
@@ -33,8 +32,8 @@ namespace Platform.Memory
         /// <include file='bin\Release\netstandard2.0\Platform.Memory.xml' path='doc/members/member[@name="P:Platform.Memory.IArrayMemory`1.Item(System.Int64)"]/*'/>
         public TElement this[long index]
         {
-            get => Pointer.GetElement(Structure<TElement>.Size, index).GetValue<TElement>();
-            set => Pointer.GetElement(Structure<TElement>.Size, index).SetValue(value);
+            get => System.Runtime.CompilerServices.Unsafe.Read<TElement>((byte*)Pointer + (System.Runtime.CompilerServices.Unsafe.SizeOf<TElement>() * index));
+            set => System.Runtime.CompilerServices.Unsafe.Write((byte*)Pointer + (System.Runtime.CompilerServices.Unsafe.SizeOf<TElement>() * index), value);
         }
 
         #endregion
@@ -55,7 +54,7 @@ namespace Platform.Memory
         /// <param name="memory"><para>An object implementing <see cref="IDirectMemory"/> interface.</para><para>Объект, реализующий интерфейс <see cref="IDirectMemory"/>.</para></param>
         public DirectMemoryAsArrayMemoryAdapter(IDirectMemory memory)
         {
-            Ensure.Always.ArgumentMeetsCriteria(m => (m.Size % Structure<TElement>.Size) == 0, memory, nameof(memory), "Memory is not aligned to element size.");
+            Ensure.Always.ArgumentMeetsCriteria(memory, m => (m.Size % Unsafe.Structure<TElement>.Size) == 0, nameof(memory), "Memory is not aligned to element size.");
             _memory = memory;
         }
 
