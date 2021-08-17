6d0334f126bd7b31caacbf568b86f1386614795b
Mitron57 <85793630+Mitron57@users.noreply.github.com>
2021-08-17 22:09:50 +0400

    Auto translation

---
+++ cpp/Platform.Memory/IMemory.h
@@ -0,0 +1,8 @@
+﻿namespace Platform::Memory
+{
+    class IMemory
+    {
+    public:
+        virtual std::int64_t Size() = 0;
+    };
+}
---
+++ cpp/Platform.Memory/TemporaryFileMappedResizableDirectMemory.h
@@ -0,0 +1,23 @@
+﻿namespace Platform::Memory
+{
+    class TemporaryFileMappedResizableDirectMemory : public FileMappedResizableDirectMemory
+    {
+        protected: override std::string ObjectName
+        {
+            get => std::string("Temporary file stored memory block at '").append(Platform::Converters::To<std::string>(Path)).append("' path.");
+        }
+
+        public: TemporaryFileMappedResizableDirectMemory(std::int64_t minimumReservedCapacity) : base(System::IO::Path::GetTempFileName(), minimumReservedCapacity) { }
+
+        public: TemporaryFileMappedResizableDirectMemory() : this(MinimumCapacity) { }
+
+        protected: void Dispose(bool manual, bool wasDisposed) override
+        {
+            base.Dispose(manual, wasDisposed);
+            if (!wasDisposed)
+            {
+                File.Delete(Path);
+            }
+        }
+    };
+}
