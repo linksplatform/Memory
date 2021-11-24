f79fc962bfa1379d3b5c400eff2a914d1e6311f9
FreePhoenix888 <freephoenix888@gmail.com>
2021-11-24 19:16:46 +0600

    Update AllTests

--- cpp/Platform.Memory.Tests/AllTests.cpp
+++ cpp/Platform.Memory.Tests/AllTests.cpp
@@ -1,38 +1,3 @@
 #include <gtest/gtest.h>
 #include <Platform.Memory.h>
-
-//#include "HeapResizableDirectMemoryTests.cpp"
-
-using namespace Platform::Memory;
-
-
-auto main() -> int {
-//
-    //FileArrayMemory<int> a("db.links");
-//
-    //auto size = sizeof(int);
-//
-    //a[0*size] = 1337;
-    //a[1*size] = 228;
-    //a[2*size] = 177013;
-    //std::cout << a[0*size] << "\n";
-    //std::cout << a[1*size] << "\n";
-    //std::cout << a[2*size] << "\n";
-
-    //HeapResizableDirectMemory a{};
-
-    //{ std::ofstream{"db.txt"}; }
-
-    //TemporaryFileMappedResizableDirectMemory a { 4096 };
-    //{ std::ofstream{"лооооол.txt"}; }
-
-    FileMappedResizableDirectMemory a { "ll.txt", 4096 };
-    DirectMemoryAsArrayMemoryAdapter<int> b(a);
-
-    b[0] = 1337;
-    b[1] = 228;
-    b[2] = 177013;
-    std::cout << b[0] << "\n";
-    std::cout << b[1] << "\n";
-    std::cout << b[2] << "\n";
-}
+#include "HeapResizableDirectMemoryTests.cpp"
