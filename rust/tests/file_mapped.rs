175792df94044d9b5e844d4e7baec2189a42a7e3
dodic <dodickgod@gmail.com>
2022-06-27 19:59:28 +0300

    extend tests

--- rust/tests/file_mapped.rs
+++ rust/tests/file_mapped.rs
@@ -13,6 +13,14 @@ fn basic() -> Result<(), Box<dyn Error>> {
     slice.iter_mut().enumerate().for_each(|(i, x)| *x = i);
 
     assert_eq!(slice, [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]);
+
+    let slice = mem.alloc(20)?;
+    slice.iter_mut().enumerate().for_each(|(i, x)| *x = i);
+
+    assert_eq!(
+        slice,
+        [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19]
+    );
     Ok(())
 }
 
